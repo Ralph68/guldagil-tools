44f99b7e7a4eb9c6cc855665753b48b1ec578e35
Ralph68 <25804416+Ralph68@users.noreply.github.com>
2025-06-22 02:29:14 +0200

    Update index.php

--- public/calculateur/index.php
+++ public/calculateur/index.php
@@ -394,8 +394,8 @@ if (!empty($preset_data['type'])) $_SESSION['calc_type'] = $preset_data['type'];
     </script>
     
     <!-- Scripts JS dans l'ordre de dépendance -->
-    <script src="../assets/js/modules/calculateur/core/state-manager.js"></script>
     <script src="../assets/js/modules/calculateur/core/calculateur-config.js"></script>
+    <script src="../assets/js/modules/calculateur/core/state-manager.js"></script>
     <script src="../assets/js/modules/calculateur/core/api-service.js"></script>
     <script src="../assets/js/modules/calculateur/models/form-data.js"></script>
     <script src="../assets/js/modules/calculateur/models/validation.js"></script>
