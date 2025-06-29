202ba5e1fca1bb84cf11d5019fafc84ca9e1a5a1
Jean Thomas <25804416+Ralph68@users.noreply.github.com>
2025-06-29 19:02:47 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -33,17 +33,17 @@ $breadcrumbs        = $breadcrumbs        ?? [
   <!-- Préchargement et inclusion du CSS principal -->
   <link
     rel="preload"
-    href="/assets/css/portal.css"
+    href="/public/assets/css/portal.css"
     as="style"
     onload="this.onload=null;this.rel='stylesheet'"
   />
   <noscript>
-    <link rel="stylesheet" href="/assets/css/portal.css" />
+    <link rel="stylesheet" href="/public/assets/css/portal.css" />
   </noscript>
 
   <!-- CSS spécifique au module -->
   <?php if (!empty($module_css)): ?>
-    <link rel="stylesheet" href="/assets/css/modules/<?= htmlspecialchars($current_module) ?>.css">
+    <link rel="stylesheet" href="/public/assets/css/modules/<?= htmlspecialchars($current_module) ?>.css">
   <?php endif; ?>
 </head>
 <body class="portal-body module-<?= htmlspecialchars($current_module) ?>" data-module="<?= htmlspecialchars($current_module) ?>">
