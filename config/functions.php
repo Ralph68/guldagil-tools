0cb53d28f94f5479f8946480678bf00ce6a9f7ce
Jean Thomas <25804416+Ralph68@users.noreply.github.com>
2025-07-13 01:52:14 +0200

    Update functions.php

--- config/functions.php
+++ config/functions.php
@@ -1,16 +1,67 @@
 <?php
-// config/functions.php
+/** 
+ * Fonctions utilitaires
+ */
 
-function logMessage($level, $message, $channel = 'app'): void {
-    // Le code de la fonction reste le même
-    if (!LOG_CONFIG['enabled']) return;
-    
-    $logFile = LOG_CONFIG['channels'][$channel] ?? LOG_CONFIG['channels']['app'];
-    $timestamp = date('Y-m-d H:i:s');
-    $logEntry = "[$timestamp] [$level] $message" . PHP_EOL;
-    
-    // ... reste du code ...
+if (!function_exists('logMessage')) {
+    function logMessage($level, $message, $channel = 'app'): void {
+        if (!LOG_CONFIG['enabled']) return;
+        
+        $logFile = LOG_CONFIG['channels'][$channel] ?? LOG_CONFIG['channels']['app'];
+        $timestamp = date('Y-m-d H:i:s');
+        $logEntry = "[$timestamp] [$level] $message" . PHP_EOL;
+        
+        $logDir = dirname($logFile);
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0755, true);
+        }
+        
+        if (file_exists($logFile) && filesize($logFile) > LOG_CONFIG['max_file_size']) {
+            $backupFile = $logFile . '.' . date('Y-m-d-H-i-s');
+            rename($logFile, $backupFile);
+        }
+        
+        file_put_contents($logFile, $logEntry, FILE_APPEND | LOCK_EX);
+    }
 }
 
-// config/config.php
-require_once 'config/functions.php';
+if (!function_exists('getFromCache')) {
+    function getFromCache($key, $default = null) {
+        if (!CACHE_CONFIG['enabled']) {
+            return $default;
+        }
+        
+        $cacheFile = CACHE_CONFIG['path'] . '/' . md5($key) . '.cache';
+        
+        if (!file_exists($cacheFile)) {
+            return $default;
+        }
+        
+        $data = unserialize(file_get_contents($cacheFile));
+        
+        if ($data['expires'] < time()) {
+            unlink($cacheFile);
+            return $default;
+        }
+        
+        return $data['value'];
+    }
+}
+
+if (!function_exists('putInCache')) {
+    function putInCache($key, $value, $ttl = null): bool {
+        if (!CACHE_CONFIG['enabled']) {
+            return false;
+        }
+        
+        $ttl = $ttl ?? CACHE_CONFIG['default_ttl'];
+        $cacheFile = CACHE_CONFIG['path'] . '/' . md5($key) . '.cache';
+        $data = [
+            'value' => $value,
+            'expires' => time() + $ttl,
+            'created' => time()
+        ];
+        
+        return file_put_contents($cacheFile, serialize($data), LOCK_EX) !== false;
+    }
+}
