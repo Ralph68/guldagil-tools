5934889cbf4c45c453c7b52c203913e1c2f54ef6
Jean Thomas <25804416+Ralph68@users.noreply.github.com>
2025-07-14 18:47:54 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -20,7 +20,7 @@ if (!defined('ROOT_PATH')) {
     //echo "ℹ️ ROOT_PATH déjà défini: " . ROOT_PATH . "\n";
 }
 
-require_once __DIR__ . '/error_handler_simple.php';
+//require_once __DIR__ . '/error_handler_simple.php';
 
 // Timezone
 date_default_timezone_set('Europe/Paris');
